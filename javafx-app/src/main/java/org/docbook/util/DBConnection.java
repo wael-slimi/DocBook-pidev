677e2b3502779392c4c7ec80cde1d6431e195d29
wael slimi <wslimi35@gmail.com>
2026-05-02 18:43:42 +0100

    change database

--- javafx-app/src/main/java/org/docbook/util/DBConnection.java
+++ javafx-app/src/main/java/org/docbook/util/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:postgresql://127.0.0.1:32769/pidev";
+    private static final String URL = "jdbc:postgresql://127.0.0.1:32770/pidev";
     private static final String USER = "pidev";
     private static final String PASSWORD = "pidev";
 
