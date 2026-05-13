f2ce228cb88ef3d6e67661a2bf5c9489408973d1
wael slimi <wslimi35@gmail.com>
2026-05-13 10:51:49 +0100

    Fix caregiver dashboard - dynamic stats and proper table structure

--- symfony-app/web/src/Controller/DashboardController.php
+++ symfony-app/web/src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PatientCaregiverRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,13 +10,13 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class DashboardController extends AbstractController
 {
-    
+
     #[Route('/dashboard', name: 'app_dashboard')]
     #[IsGranted('IS_AUTHENTICATED_FULLY')] // Ensures user is logged in
     public function index(): Response
     {
         $user = $this->getUser();
-        
+
         // 1. Check if user is a Doctor
         if ($this->isGranted('ROLE_DOCTOR')) {
             return $this->render('doctor/dashboard.html.twig');
@@ -23,7 +24,13 @@ class DashboardController extends AbstractController
 
         // 2. Check if user is a Caregiver
         if ($this->isGranted('ROLE_CAREGIVER')) {
-            return $this->render('caregiver/dashboard.html.twig');
+            /** @var \App\Entity\Caregiver $caregiver */
+            $caregiver = $user;
+            $managedPatients = $caregiver->getPatientCaregivers()->map(fn($pc) => $pc->getPatient())->filter(fn($p) => $p !== null)->toArray();
+
+            return $this->render('caregiver/dashboard.html.twig', [
+                'managedPatients' => $managedPatients,
+            ]);
         }
 
         // 3. Default to Patient Dashboard
@@ -40,14 +47,20 @@ class DashboardController extends AbstractController
      * Direct link to test Caregiver UI: http://localhost:8000/test/caregiver
      */
     #[Route('/test/caregiver', name: 'test_caregiver')]
-    public function testCaregiver(): Response
+    public function testCaregiver(PatientCaregiverRepository $pcRepo): Response
     {
-        return $this->render('caregiver/dashboard.html.twig');
+        /** @var \App\Entity\Caregiver $caregiver */
+        $caregiver = $this->getUser();
+        $managedPatients = $caregiver->getPatientCaregivers()->map(fn($pc) => $pc->getPatient())->filter(fn($p) => $p !== null)->toArray();
+
+        return $this->render('caregiver/dashboard.html.twig', [
+            'managedPatients' => $managedPatients,
+        ]);
     }
     #[Route('/test/patient', name: 'test_patient')]
     public function testPatient(): Response
     {
         return $this->render('patient/dashboard.html.twig');
     }
-    
+
 }
